1afe6597429f51f79172323b122c0fae01c4b494
ChenMT <gene31105@gmail.com>
2016-09-30 14:35:43 -0700

    resolve new wp version file path

--- README.md
+++ README.md
@@ -1,30 +1,37 @@
-#TheDirtySix
+# WordPress Skeleton #
 
+## Downloading This repository ##
+You can get this to work in two ways, via `.git` or download it manually
 
+### Donwloading via Git ###
+In the webroot run:
 
+````git clone https://github.com/janw-oostendorp/WordPress-Skeleton.git .````
 
-## WordPress BASE ON "Skeleton"
+This will download the base, next we download the WordPress submodule.
 
-* More easy to work on WP
+````git submodule update --init````
 
-## Assumptions
+If needed add your own remote repository
 
-* `/content/` and `wp-config.php` add to gitignore after first deploy
-* Custom content directory in `/content/` (cleaner, and also because it CAN'T be in `/wp/`)
-* `wp-config.php` in the root (because it can't be in `/wp/`)
+````git remote rm origin````
+````git remote add origin YOUR_REPOSITORY.URL/````
 
+### Manual download ###
+Download the [zip](https://github.com/janw-oostendorp/WordPress-Skeleton/archive/master.zip) and unzip it in the webroot. Download [Wordpress](http://wordpress.org/latest.zip) unzip it in the `/wp` folder.
 
-## Guides
+## How to install ##
+After downloading you can continue to install WordPress
+1) Copy `local-config.sample.php` and rename it to `local-config.php`
+2) Add the database constants and the domain
+3) In `wp-config.php` Edit the `$table_prefix`
+4) add the `*_KEY` constants which can be generated [here](https://api.wordpress.org/secret-key/1.1/salt)
+5) open your website and continue with the famous 5 minute install.
 
-* Delete and add new database if any error occurs
-* Wordpress version 4.6.1
-* Ready to Start
+## Benefits ##
 
+- WordPress is in it's own folder `/wp`.
+- Webroot only has two folders `/wp` and `/content`
+- Automatic updates are enabled for production, adding more security.
+- Error display and logging are setup. On production errors will be logged, on test the will be shown.
 
-##Workflow
-
-1.Pull
-2.Create new branch
-3.Push to new branch
-4.Commit branch
-5.Pull request
--- wp-config.php
+++ wp-config.php
@@ -1,83 +1,84 @@
 <?php
-// ===================================================
-// Load database info and local development parameters
-// ===================================================
+// Check if a local development file is defined and include it.
 if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
-	define( 'WP_LOCAL_DEV', true );
 	include( dirname( __FILE__ ) . '/local-config.php' );
-} else {
-	define( 'WP_LOCAL_DEV', false );
-	define( 'DB_NAME', 'wp' );
-	define( 'DB_USER', 'root' );
-	define( 'DB_PASSWORD', 'root' );
-	define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
 }
 
-// ========================
-// Custom Content Directory
-// ========================
-define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
-define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
+//====================================================================
+// Fill in all the needed constants below
+//====================================================================
 
-// ================================================
-// You almost certainly do not want to change these
-// ================================================
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
+// Database
+defined( 'DB_NAME' )     or define( 'DB_NAME', '%%' );
+defined( 'DB_USER' )     or define( 'DB_USER', '%%' );
+defined( 'DB_PASSWORD' ) or define( 'DB_PASSWORD', '%%' );
+defined( 'DB_HOST' )     or define( 'DB_HOST', 'localhost' );
+// ftp
+defined( 'FTP_USER' ) or define( 'FTP_USER', '%%' );
+defined( 'FTP_PASS' ) or define( 'FTP_PASS', '%%' );
+defined( 'FTP_HOST' ) or define( 'FTP_HOST', '%%' );
+defined( 'FTP_SSL' )  or define( 'FTP_SSL', false );
+// Url
+defined( 'WP_HOME' )  or define( 'WP_HOME', 'http://example.com' ); // no trailing slash
 
-// ==============================================================
-// Salts, for security
-// Grab these from: https://api.wordpress.org/secret-key/1.1/salt
-// ==============================================================
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+$table_prefix = 'rndm_'; // please change with 2-5 random letters/digits
 
-// ==============================================================
-// Table prefix
-// Change this if you have multiple installs in the same database
-// ==============================================================
-$table_prefix  = 'wp_';
+// https://api.wordpress.org/secret-key/1.1/salt
+define( 'AUTH_KEY', '%%' );
+define( 'SECURE_AUTH_KEY', '%%' );
+define( 'LOGGED_IN_KEY', '%%' );
+define( 'NONCE_KEY', '%%' );
+define( 'AUTH_SALT', '%%' );
+define( 'SECURE_AUTH_SALT', '%%' );
+define( 'LOGGED_IN_SALT', '%%' );
+define( 'NONCE_SALT', '%%' );
 
-// ================================
-// Language
-// Leave blank for American English
-// ================================
-define( 'WPLANG', '' );
+// Multisite
+defined( 'WP_ALLOW_MULTISITE' )   or define( 'WP_ALLOW_MULTISITE', false );
+/* // Unquote for multisites
+defined( 'MULTISITE' )            or define( 'MULTISITE', true );
+defined( 'SUBDOMAIN_INSTALL' )    or define( 'SUBDOMAIN_INSTALL', false );
+defined( 'DOMAIN_CURRENT_SITE' )  or define( 'DOMAIN_CURRENT_SITE', 'example.com' ); // no `http://` see local-config.php
+defined( 'PATH_CURRENT_SITE' )    or define( 'PATH_CURRENT_SITE', '/' );
+defined( 'SITE_ID_CURRENT_SITE' ) or define( 'SITE_ID_CURRENT_SITE', 1 );
+defined( 'BLOG_ID_CURRENT_SITE' ) or define( 'BLOG_ID_CURRENT_SITE', 1 );
+/**/
 
-// ===========
-// Hide errors
-// ===========
-ini_set( 'display_errors', 0 );
-define( 'WP_DEBUG_DISPLAY', false );
+//====================================================================
+// That's all, stop editing! Happy blogging.
+//====================================================================
+// Minor tweaks
+defined( 'AUTOSAVE_INTERVAL' )  or define( 'AUTOSAVE_INTERVAL', 300 ); // autosave every 300 seconds
+defined( 'WP_POST_REVISIONS' )  or define( 'WP_POST_REVISIONS', 10 ); // 10 post revisions
+defined( 'DISALLOW_FILE_EDIT' ) or define( 'DISALLOW_FILE_EDIT', true ); // don't allow to edit files in the wp-admin
 
-// =================================================================
-// Debug mode
-// Debugging? Enable these. Can also enable them in local-config.php
-// =================================================================
-// define( 'SAVEQUERIES', true );
-// define( 'WP_DEBUG', true );
+// URL and dirs
+defined( 'WP_SITEURL' )     or define( 'WP_SITEURL', WP_HOME . '/wp' );
+defined( 'WP_CONTENT_DIR' ) or define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
+defined( 'WP_CONTENT_URL' ) or define( 'WP_CONTENT_URL', WP_HOME . '/content' );
+defined( 'PLUGINDIR' )      or define( 'PLUGINDIR', 'content/plugins' ); // Relative to ABSPATH. For back compat.
+defined( 'MUPLUGINDIR' )    or define( 'MUPLUGINDIR', 'content/mu-plugins' ); // Relative to ABSPATH. For back compat.
 
-// ======================================
-// Load a Memcached config if we have one
-// ======================================
-if ( file_exists( dirname( __FILE__ ) . '/memcached.php' ) )
-	$memcached_servers = include( dirname( __FILE__ ) . '/memcached.php' );
+// Debug turned off on production
+defined( 'WP_DEBUG' ) or define( 'WP_DEBUG', false );
 
-// ===========================================================================================
-// This can be used to programatically set the stage when deploying (e.g. production, staging)
-// ===========================================================================================
-define( 'WP_STAGE', '%%WP_STAGE%%' );
-define( 'STAGING_DOMAIN', '%%WP_STAGING_DOMAIN%%' ); // Does magic in WP Stack to handle staging domain rewriting
+// log errors when it's not a development server
+if ( ! defined( 'WP_DEVELOPMENT' ) || WP_DEVELOPMENT !== true || ! defined( 'WP_DEVELOPMENT' ) || WP_DEBUG_DISPLAY === false ) {
+	defined( 'WP_DEBUG_LOG' )     or define( 'WP_DEBUG_LOG', true );
+	defined( 'WP_DEBUG_DISPLAY' ) or define( 'WP_DEBUG_DISPLAY', false );
+	@ini_set( 'display_errors', 0 );
+	@ini_set( 'log_errors', 1 );
+	@ini_set( 'error_log', WP_CONTENT_DIR . '/debug.log' );
+}
 
-// ===================
-// Bootstrap WordPress
-// ===================
-if ( !defined( 'ABSPATH' ) )
-	define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
+// DB
+defined( 'DB_CHARSET' ) or define( 'DB_CHARSET', 'utf8' );
+defined( 'DB_COLLATE' ) or define( 'DB_COLLATE', '' );
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	defined( 'ABSPATH' ) or define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );

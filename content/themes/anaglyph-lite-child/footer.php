f18abeac9a8cf9781c83c705f1f950ffd8b99838
sethperlstein <sethperlstein@gmail.com>
2016-10-23 17:55:39 -0700

    Installed plugins and created child theme, page template and footer
    Child theme for Anaglyph Lite

---
+++ content/themes/anaglyph-lite-child/footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package WordPress
+ * @subpackage Anaglyph-Lite-Child
+ * @since Anaglyph Lite 1.0
+ */
+?>
+
+<footer>
+<p>FOLLOW US (social links)</p>
+<small>© 2016 AMPLIFIED VOICES</small>
+<p>info@amplifiedvoices.com</p>
+<p>phone number</p>
+<p>address</p>
+AV LOGO
+
+</footer>
